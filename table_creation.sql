f2bd58a1539d6167c2878aa789d7b45c00be66f3
Mitchell Meier <mitchhit@gmail.com>
2021-06-03 16:11:49 -0500

    inital table creation sql with transaction table

---
+++ table_creation.sql
@@ -0,0 +1,15 @@
+/* Column num is a value that is left blank in the original csv, so I decided 
+to make num a counter that increases by 1 for each transation
+This will make it easier for us to determine which transactions happened in what
+order, since datetime is not used by commerce and we could have transactions
+on the same date */
+
+CREATE TABLE TRANSACTIONS (
+  date DATE NOT NULL,
+  num INT UNSIGNED NOT NULL,
+  description CHAR NOT NULL,
+  debit INT UNSIGNED,
+  credit INT UNSIGNED,
+  CONSTRAINT CHK_NET CHECK (debit IS NULL or credit IS NULL),
+  PRIMARY KEY (date,num)
+)
