cc59eb8d2cfcf45a1af7114a2f3c41b656ec8c24
Mitchell Meier <mitchhit@gmail.com>
2021-06-13 18:55:31 -0500

    ditched matplot, switched implementation over to plotly, and implemented graph by date

--- graph.py
+++ graph.py
@@ -3,6 +3,11 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import db_create as db
 
+import plotly.offline as py
+import pandas as pd
+
+
+
 
 #Default filenames, change as needed
 DB_NAME = "transaction.db"
@@ -23,35 +28,60 @@ def generate_plot(X,Y,x_label,y_label,title):
 #Given our current value, it gives our starting value
 #before the earliest transaction recorded
 def fetch_starting(D,current):
-  for i in D:
-    current -= net_value(i)
+  for _ , row in D.iterrows():
+    current -= row['net']
   #Round in case of floating point arithmetic getting wonky
   return round(current,2)
 
 
-#Will return the credit value as positive
-#or debit value as negative
-def net_value(row):
-  if row[3]:
-    return -row[3]
-  return row[4]
+def fetch_current(D,current):
+  ret = []
+  for _, row in D.iterrows():
+    current += row['net']
+    ret.append(current)
+  return ret
+  
+#Since date information from Commerce does not contain time
+#I use this function to prevent transactions from stacking on top 
+#of each other in our graphs by seperating them with hour variables
+#Though not accurate to the hours IRL, they still contain correct order
+def adjust_dates(D):
+  ret = []
+  current, count = "", 0
+  for j, row in D.iterrows():
+    if row['date'] == current:
+      count += 1
+    if row['date'] != current or j == len(D):
+      denom = count + 1
+      for i in range(1,denom):
+        hr_extension = int((i/denom)*24)
+        ret.append(current + " " + str(hr_extension))
+
+      current = row['date']
+      count = 1
+    
+      if j == len(D)-1:
+        denom = count + 1
+        for i in range(1,denom):
+          hr_extension = int((i/denom)*24)
+          ret.append(current + " " + str(hr_extension))
+
+
+
+  return ret
+
+
 
 
 #Plotting transaction number against 
 #current value, given a list of transactions
 #and the starting balance before those transactions
-def plot_by_transaction_num(D,current):
-  x = [0]
-  y = [current]
-  for i in D:
-    current += net_value(i)
-    y.append(current)
-    x.append(i[1])
-  
-  generate_plot(x,y,'Transaction Number',
-    'Current Balance','Account Transactions')
-
-  plt.show()
+def plot_by_date(D):
+  py.plot([{
+    'x': D['date'],
+    'y': D[col],
+    'name': col
+  } for col in D.columns[3:] if col != 'net'])
 
 
 
@@ -67,10 +97,17 @@ if __name__ == "__main__":
   cursor.execute(statement)
   data = cursor.fetchall()
 
+  df = pd.read_sql_query(statement,conn)
+  df = df.fillna(0)
+  df['net'] = df['credit'] - df['debit']
+
   #Starting value, i.e. the account balance before 
   #our first transaction stored in the database
-  start = fetch_starting(data[::-1], CURRENT_VALUE)
+  start = fetch_starting(df.iloc[::-1], CURRENT_VALUE)
+  df['current'] = fetch_current(df,start)
   
-  plot_by_transaction_num(data,start)
+  df['date'] = adjust_dates(df)
+
+  plot_by_date(df)
 
 
