174250cae58b4a389b93bf194a9e9b5650566ab5
Mitchell Meier <mitchhit@gmail.com>
2021-06-18 19:24:31 -0500

    restructure and create new paths

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+from re import L
 from flask import Flask, render_template, redirect, url_for, request
 import graph as g
 
 
 app = Flask(__name__)
 
+DF = g.initalize()
 
 @app.route('/')
 def home():
-  pl = g.main()
-  return render_template("welcome.html",plot=pl)
+  pl = g.balance_plot(DF)
+  return render_template("index.html",plot=pl)
+
+@app.route('/debit')
+def debit():
+  pl = g.balance_graph()
+  return render_template("index.html",plot=pl)
 
 
 if __name__ == "__main__":
