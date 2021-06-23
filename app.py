bfa7ca9ab21823079373013e81498903e47ed4eb
Mitchell Meier <mitchhit@gmail.com>
2021-06-22 21:13:58 -0500

    porting html layout to dash application, as well as changing style on navbar and responsive graph

--- app.py
+++ app.py
@@ -1,58 +1,175 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash_html_components.A import A
+from dash_html_components.Img import Img
+from dash_html_components.Li import Li
+from dash_html_components.Ul import Ul
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+import graph as gp
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+css = [dbc.themes.BOOTSTRAP, 'static/styles.css']
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+app = dash.Dash(__name__, external_stylesheets=css)
 
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+df = gp.initalize()
+
+fig = px.scatter(df, x='date', y='current', custom_data=['net'])
+
+
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Home", href="#")),
+        dbc.NavItem(dbc.NavLink("Debits", href="/debit")),
+        dbc.NavItem(dbc.NavLink("Credits", href="/credit")),
+        dbc.NavItem(dbc.NavLink("Net Balance", href="/net")),
+    ],
+    color="#17a2b8",
+    dark=True,
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
+app.layout = html.Div(
+  className='grid-row-container',
+  children=[
+    html.Div(
+      className='grid-row-child',
+      children=[
+        html.Nav(
+          className='navbar navbar-expand-sm navbar-light bg-light',
+          children=[
+            html.A(
+              className='navbar-brand',
+              href='/',
+              children='Commerce GUI'
+            ),              
+            html.Div(
+              className='collapse navbar-collapse',
+              children=[
+                html.Ul(
+                  className='navbar-nav',
+                  children=[
+                    html.Li( 
+                      className='nav-item',
+                      children=[
+                        html.A(
+                          className='nav-link',
+                          href='/',
+                          children='Home'
+                        )
+                      ]
+                    ),
+                    html.Li( 
+                      className='nav-item',
+                      children=[
+                        html.A(
+                          className='nav-link',
+                          href='/debit',
+                          children='Debits'
+                        )
+                      ]
+                    ),
+                    html.Li( 
+                      className='nav-item',
+                      children=[
+                        html.A(
+                          className='nav-link',
+                          href='/credit',
+                          children='Credits'
+                        )
+                      ]
+                    ),
+                    html.Li( 
+                      className='nav-item',
+                      children=[
+                        html.A(
+                          className='nav-link',
+                          href='/net',
+                          children='Net Balance'
+                        )
+                      ]
+                    ),
+                  ]
+                )
+              ]
+            ),
+            html.Div(
+              className='collapse navbar-collapse justify-content-end',
+              children=[
+                html.Ul(
+                  className='navbar-nav',
+                  children=[ 
+                    html.Li(
+                      className='nav-item',
+                      children=[ 
+                        html.A(
+                          className='nav-link',
+                          href='https://github.com/mitchhit234/commerceGUI',
+                          children=[ 
+                            html.Img(
+                              src='static/github.png',
+                              width=40,
+                              height=40
+                            )
+                          ]
+                        )
+                      ]
+                    )
+                  ]
+                )
+              ]
+            )
+          ]
+        )
+      ]
     ),
-
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
+    html.Div(
+      className='grid-row-child',
+      children=[
+        html.Div(
+          className='grid-col-container',
+          children=[
+            html.Div(
+              className='grid-col-child',
+              children=dcc.Graph(
+                id='main',
+                figure=fig,
+                style=dict(width='100%',height='100%'),
+                responsive=True    
+              )
+            ),
+            html.Div(
+              className='grid-col-child',
+              children='Grid column 2'
+            )
+          ]
+        )
+      ]
     )
-])
+  ]
+)
+
+
+
+
+
+
+
+
+
+
+# html.Div([
+#   dcc.Graph(
+#     id='main',
+#     figure=fig
+#   )])
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+  app.run_server(debug=True)
