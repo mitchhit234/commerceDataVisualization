d52a76339c75cb09a0953fd0ea299ba76c4ce840
Mitchell Meier <mitchhit@gmail.com>
2021-07-05 13:02:30 -0500

    prevent some pip errors

--- run.sh
+++ run.sh
@@ -4,10 +4,9 @@
 #assumes you have python3 and pip installed
 if [[ $1 = '-s' ]]
 then
-	pip install -r resources/requirements.txt
+	python3 -m pip install -r resources/requirements.txt
 	python3 python_scripts/db_create.py
 fi
 
-python3 python_scripts/db_update.py
 python3 python_scripts/app.py
 
